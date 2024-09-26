8f638fcd3282624769f07e21cf2fe8da23505b3a
Leonardo Ferreira <eng.leonardo.abreu@gmail.com>
2024-09-26 11:52:05 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,17 @@
 package application;
 
-import java.time.LocalDate;
-
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+				
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Department dp = new Department(1, "Books");
-		System.out.println(dp);
-	
-		Seller seller = new Seller(1, "Bob", "bob@gmail.com", LocalDate.now(), 3000.0, dp);
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 	}
 
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -1,7 +1,8 @@
 package model.entities;
 
 import java.io.Serializable;
-import java.time.LocalDate;
+import java.util.Date;
+//import java.sql.Date;
 import java.util.Objects;
 
 public class Seller implements Serializable{
@@ -11,12 +12,12 @@ public class Seller implements Serializable{
 	private Integer id;
 	private String name;
 	private String email;
-	private LocalDate birthDate;
+	private Date birthDate;
 	private Double baseSalary;
 	
 	private Department department;
 	
-	public Seller(Integer id, String name, String email, LocalDate birthDate, Double baseSalary, Department department) {
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -53,11 +54,11 @@ public class Seller implements Serializable{
 		this.email = email;
 	}
 
-	public LocalDate getBirthDate() {
+	public Date getBirthDate() {
 		return birthDate;
 	}
 
-	public void setBirthDate(LocalDate date) {
+	public void setBirthDate(Date date) {
 		birthDate = date;
 	}
 
