0c8c013cb035432fc3c6d717dda6d968476ae763
Leonardo Ferreira <eng.leonardo.abreu@gmail.com>
2024-09-28 12:29:13 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,7 +13,9 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-				
+		
+		Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("=== TEST 1: seller findById ===");
@@ -42,6 +46,13 @@ public class Program {
 		seller.setName("Martha Wayne");
 		sellerDao.update(seller);
 		System.out.println("Update completed!");
+		
+		System.out.println("\n=== TEST 6: seller delete ===");
+		System.out.println("Enter id for delete teste: ");
+		int id = scanner.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed!");
+		scanner.close();
 		
 	}
 
